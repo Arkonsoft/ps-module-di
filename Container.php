1ab173c10d401530bfc93a308f37d103a09ee6c6
Grzegorz Wójcik <gwojcik@arkonsoft.pl>
2023-09-11 14:15:53 +0200

    add Container interface

--- Container.php
+++ Container.php
@@ -24,7 +24,7 @@ if (!defined('_PS_VERSION_')) {
     exit;
 }
 
-class Container
+class Container implements ContainerInterface
 {
     private static $services = [];
 
---
+++ ContainerInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * NOTICE OF LICENSE
+ *
+ * This file is licensed under the Software License Agreement.
+ *
+ * With the purchase or the installation of the software in your application
+ * you accept the license agreement.
+ *
+ * You must not modify, adapt or create derivative works of this source code
+ *
+ * @author Arkonsoft
+ * @copyright 2023 Arkonsoft
+ */
+
+declare(strict_types=1);
+
+namespace Arkonsoft\PsModule\DI;
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+interface ContainerInterface
+{
+    public function get(string $name): ServiceInterface;
+    public function set(string $name, string $class, array $params = []): bool;
+}
